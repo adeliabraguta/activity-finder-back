3d413040a05bf3aec3e6376e2a64f69430731b6d
adeliabraguta <adelia.braguta@isa.utm.md>
2024-05-14 12:29:26 +0300

    Add swagger

---
+++ routes/activities.js
@@ -0,0 +1,242 @@
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Activity:
+ *       type: object
+ *       required:
+ *         - activity
+ *         - accessibility
+ *         - type
+ *         - participants
+ *         - price
+ *         - link
+ *       properties:
+ *         activity:
+ *           type: string
+ *           description: The title of activity
+ *         accessibility:
+ *           type: number
+ *           description: The price of the activity
+ *         type:
+ *           type: string
+ *           description: The price of the activity
+ *         participants:
+ *           type: number
+ *           description: The price of the activity
+ *         price:
+ *           type: number
+ *           description: The price of the activity
+ *         link:
+ *           type: string
+ *           description: The price of the activity
+ *       example:
+ *         activity: Read a book
+ *         accessibility: 2
+ *         type: educational
+ *         participants: 1
+ *         price: 0
+ *         link: ''
+ *     Login:
+ *       type: object
+ *       required:
+ *         - username
+ *         - password
+ *       properties:
+ *         username:
+ *           type: string
+ *           description: Username of the user
+ *         password:
+ *           type: string
+ *           description: Password of the user
+ *       example:
+ *         username: admin
+ *         password: adminadmin
+ *     Update-Activity:
+ *       type: object
+ *       properties:
+ *         activity:
+ *           type: string
+ *           description: The title of activity
+ *         accessibility:
+ *           type: number
+ *           description: The price of the activity
+ *         type:
+ *           type: string
+ *           description: The price of the activity
+ *         participants:
+ *           type: number
+ *           description: The price of the activity
+ *         price:
+ *           type: number
+ *           description: The price of the activity
+ *         link:
+ *           type: string
+ *           description: The price of the activity
+ *       example:
+ *         activity: Read a book
+ *         accessibility: 2
+ *         type: educational
+ *         participants: 1
+ *         price: 0
+ *         link: ''
+ */
+
+
+/**
+ * @swagger
+ * /activities:
+ *   get:
+ *     security:
+ *       - Authorization: []
+ *     summary: Get activities
+ *     parameters:
+ *         - in: query
+ *           name: page
+ *           schema:
+ *             type: integer
+ *             default: 1
+ *           description: Page number of the activities list
+ *         - in: query
+ *           name: limit
+ *           schema:
+ *             type: integer
+ *             default: 10
+ *           description: Number of activities per page
+ *     responses:
+ *       200:
+ *         description: Activities list.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 totalItems:
+ *                   type: integer
+ *                   example: 5
+ *                 totalPages:
+ *                   type: integer
+ *                   example: 2
+ *                 currentPage:
+ *                   type: integer
+ *                   example: 1
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     $ref: '#/components/schemas/Activity'
+ *       401:
+ *         description: Authentication error, token is expired or invalid.
+ *       403:
+ *         description: Authentication error, token is valid but the user does not have the required role.
+ *       500:
+ *         description: Server Error
+ *
+ */
+
+/**
+ * @swagger
+ * /login:
+ *   post:
+ *     summary: Login with role
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Login'
+ *     responses:
+ *       200:
+ *         description: Token created.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Login'
+ *       401:
+ *         description: Authentication error, token is expired or invalid.
+ *       403:
+ *         description: Authentication error, token is valid but the user does not have the required role.
+ *       500:
+ *         description: Server Error
+ *
+ */
+
+/**
+ * @swagger
+ * /post-activity:
+ *   post:
+ *     security:
+ *       - Authorization: []
+ *     summary: Post activity
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Activity'
+ *     responses:
+ *       200:
+ *         description: Activity created.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Activity'
+ *       400:
+ *         description: Bad request,Missing required fields.
+ *       401:
+ *         description: Authentication error, token is expired or invalid.
+ *       403:
+ *         description: Authentication error, token is valid but the user does not have the required role.
+ *       500:
+ *         description: Server Error
+ *
+ */
+
+/**
+ * @swagger
+ * /delete-activity/:id:
+ *   delete:
+ *     security:
+ *       - Authorization: []
+ *     summary: Delete activity
+ *     responses:
+ *       200:
+ *         description: Activity deleted.
+ *       401:
+ *         description: Authentication error, token is expired or invalid.
+ *       403:
+ *         description: Authentication error, token is valid but the user does not have the required role.
+ *       500:
+ *         description: Server Error
+ *
+ */
+
+/**
+ * @swagger
+ * /update-activity/:id:
+ *   put:
+ *     security:
+ *       - Authorization: []
+ *     summary: Update activity
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Update-Activity'
+ *     responses:
+ *       200:
+ *         description: Activity created.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Update-Activity'
+ *       400:
+ *         description: Bad request.
+ *       401:
+ *         description: Authentication error, token is expired or invalid.
+ *       403:
+ *         description: Authentication error, token is valid but the user does not have the required role.
+ *       500:
+ *         description: Server Error
+ *
+ */
--- server.js
+++ server.js
@@ -2,6 +2,9 @@ import {config} from "dotenv";
 import express from 'express'
 import methods from "./routes/api.js";
 import mongoose from "mongoose";
+import swaggerUi from "swagger-ui-express";
+import swaggerJsdoc from 'swagger-jsdoc';
+import cors from 'cors'
 
 config()
 const app = express()
@@ -9,8 +12,39 @@ const app = express()
 await mongoose.connect('mongodb://127.0.0.1:27017/myapp');
 mongoose.set('bufferCommands', false);
 
+const options = {
+    definition: {
+        openapi: "3.1.0",
+        info: {
+            title: "Activity Finder API",
+            version: "0.1.0",
+            description:
+                "This is a simple CRUD API application made with Express and documented with Swagger"
+        },
+        components: {
+            securitySchemes: {
+                Authorization: {
+                    type: "http",
+                    scheme: "bearer",
+                    bearerFormat: "JWT",
+                    value: "Bearer <JWT token here>"
+                }
+            }
+        },
+        servers: [
+            {
+                url: "http://localhost:3000/api",
+            },
+        ],
+    },
+    apis: ["./routes/*.js"],
+};
+app.use(cors());
+app.use("/api-docs",
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerJsdoc(options)))
 app.use(express.json());
-app.use('/api', methods)
+app.use(methods)
 
 app.listen(3000)
 
